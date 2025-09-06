8c44cafcfd6e82b7b28ef1126dac1eb827d57cf1
Ran <razaanarjuna@gmail.com>
2025-09-06 11:50:54 +0700

    Add goat patrol logic and improve scene setup
    Introduced patrol variables and boundaries in goat.gd, and added animation switching for walking and idle states. Updated goat.tscn to expand walk animation frames and use an external collision shape resource. Added new goatcol.tres for collision, improved game.tscn with side boundaries, and set project window to 1920x1080, borderless, and transparent. Removed unused yyyyy.png asset.

--- Codes/game.gd
+++ Codes/game.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+#@onready var goat_sprite = $GoatSprite from goat tscn
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Set background to be transparent
 	get_tree().get_root().set_transparent_background(true)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	# Fix this thing
+	#set_passthrough(goat_sprite)
+
+#func set_passthrough(sprite: AnimatedSprite2D):
+	#var texture_center: Vector2 = sprite.texture.get_size() / 2
+	#var texture_corners: PackedVector2Array = [
+		#sprite.global_position + texture_center * Vector2(-1, -1), # Top left corner
+		#sprite.global_position + texture_center * Vector2(1, -1), # Top right corner
+		#sprite.global_position + texture_center * Vector2(1 , 1), # Bottom right corner
+		#sprite.global_position + texture_center * Vector2(-1 ,1) # Bottom left corner
+	#]
+#
+	#DisplayServer.window_set_mouse_passthrough(texture_corners)
--- Codes/goat.gd
+++ Codes/goat.gd
@@ -4,6 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var start_position: Vector2
+@export var patrol_distance = 200.0 # Try to randomize it
+var left_limit: float
+var right_limit: float
+
+func _ready():
+	start_position = global_position
+	left_limit = start_position.x - patrol_distance / 2
+	right_limit = start_position.x + patrol_distance / 2	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,6 +27,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		$GoatSprite.play("walk")
 		if direction < 0:
 			$GoatSprite.flip_h = true
 			velocity.x = direction * SPEED
@@ -25,6 +35,27 @@ func _physics_process(delta):
 			$GoatSprite.flip_h = false
 			velocity.x = direction * SPEED
 	else:
+		$GoatSprite.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#func patrol_movement():
+	## Move in patrol direction
+	#velocity.x = patrol_direction * patrol_speed
+#
+	## Turn around if we reach the patrol boundaries
+	#if patrol_direction < 0 and global_position.x <= left_limit:
+		#patrol_direction = 0
+		#await get_tree().create_timer(1.0).timeout
+		#patrol_direction = 1
+	#elif patrol_direction > 0 and global_position.x >= right_limit:
+		#patrol_direction = 0
+		#await get_tree().create_timer(1.0).timeout
+		#patrol_direction = -1
+	#
+	## Flip sprite based on movement direction
+	#if patrol_direction > 0:
+		#animated_sprite.flip_h = false
+	#elif patrol_direction < 0:
+		#animated_sprite.flip_h = true
