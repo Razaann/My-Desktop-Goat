737125a0e7a6208393d6bc2e69cbdc34e9bf42c9
Ran <razaanarjuna@gmail.com>
2025-09-08 14:31:03 +0700

    Add pink goat character and new game scene
    Introduces a new pink goat sprite and updates goat.tscn to use it. Adds game_2.gd and game_2.tscn for a new game mode with horizontal movement and direction flipping. Updates game_1.gd and game_1.tscn for goat scaling and positioning, and adjusts project.godot viewport width for improved layout.

--- Codes/game_1.gd
+++ Codes/game_1.gd
@@ -4,7 +4,11 @@ extends Node2D
 @onready var win := get_window()
 
 # goat is 48x48, window is 64x64, so offset centers the sprite 
-var offset := Vector2i(24, 922) 
+# For goat that scale 2
+#var offset := Vector2i(-20, 922)
+
+# For goat that scale 3
+var offset := Vector2i(-20, 932) 
 
 func _physics_process(delta):
 	# NEED TO FIX THE DELAY WINDOWS
